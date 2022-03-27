0b7f9c10ea7de12a1fbab16d7524a7480bbb2776
Gabriele Teotino <gabriele@teosoft.it>
2022-03-27 11:26:15 +0100

    Use refresh instead of update

--- FunctionsPipeline.cs
+++ FunctionsPipeline.cs
@@ -17,32 +17,32 @@ namespace Homecloud
     {
         [FunctionName("RefreshPipelinesRest")]
         public static IActionResult SendRefreshPipelinesCommand(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "UpdatePipelines")] RefreshPipelineRequest updatePipelineRequest,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "RefreshPipelines")] RefreshPipelineRequest refreshPipelineRequest,
             [Queue("refresh-pipelines")] out RefreshPipelinesCommand refreshPipelinesCommand,
             ILogger logger)
         {
-            logger.LogInformation("SendUpdatePipelinesCommand - C# HTTP trigger function processed a request.");
-            logger.LogDebug($"Request: {JsonConvert.SerializeObject(updatePipelineRequest)}");
+            logger.LogInformation("SendRefreshPipelinesCommand - C# HTTP trigger function processed a request.");
+            logger.LogDebug($"Request: {JsonConvert.SerializeObject(refreshPipelineRequest)}");
 
-            refreshPipelinesCommand = new(updatePipelineRequest.ProjectHash, updatePipelineRequest.ApiUrl);
+            refreshPipelinesCommand = new(refreshPipelineRequest.ProjectHash, refreshPipelineRequest.ApiUrl);
             logger.LogDebug($"Command: {JsonConvert.SerializeObject(refreshPipelinesCommand)}");
 
             return new OkResult();
         }
 
         [FunctionName("RefreshPipelines")]
-        public static async Task ProcessUpdatePipelinesCommand(
+        public static async Task ProcessRefreshPipelinesCommand(
             [QueueTrigger("refresh-pipelines")] RefreshPipelinesCommand refreshPipelinesCommand,
             IBinder binder,
             ILogger logger)
         {
-            logger.LogInformation($"ProcessUpdatePipelinesCommand - C# Queue trigger function processing command: {JsonConvert.SerializeObject(refreshPipelinesCommand)}");
+            logger.LogInformation($"ProcessRefreshPipelinesCommand - C# Queue trigger function processing command: {JsonConvert.SerializeObject(refreshPipelinesCommand)}");
             var personalAccessToken = Environment.GetEnvironmentVariable("DevOpsPat", EnvironmentVariableTarget.Process);
             var client = new DevopsApiClient(personalAccessToken, refreshPipelinesCommand.ApiUrl, logger);
             var pipelines = await client.ListPipelines();
             logger.LogInformation("Pipelines data received");
 
-            using var writer = await binder.BindAsync<TextWriter>(new BlobAttribute($"pipelines-data/{refreshPipelinesCommand.ProjectHash}", FileAccess.Write));
+            using var writer = await binder.BindAsync<TextWriter>(new BlobAttribute($"pipelines-data/{refreshPipelinesCommand.ProjectHash}.json", FileAccess.Write));
             await writer.WriteAsync(JsonConvert.SerializeObject(pipelines));
 
             logger.LogInformation("Pipelines data saved");
